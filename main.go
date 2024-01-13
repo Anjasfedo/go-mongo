86ae497acc1171ecc040f72f260c3edb2ed72398
AnjasFedo <fedoaf98@gmail.com>
2024-01-13 19:39:40 +0700

    Connect to mongoDB

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"net/http"
 
+	"github.com/Anjasfedo/go-mongo/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
-	
+	r := httprouter.New()
+
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("")
+
+	r.GET("")
+
+	r.DELETE("")
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+
+	return s
 }
--- models/user.go
+++ models/user.go
@@ -1,5 +1,12 @@
 package models
 
 import (
-	
+	"gopkg.in/mgo.v2/bson"
 )
+
+type User struct {
+	ID     bson.ObjectId `json:"ID" bson:"_ID"`
+	Name   string        `json:"name" bson:"name"`
+	Gender string        `json:"gender" bson:"gender"`
+	Age    int           `json:"age" bson:"age"`
+}
