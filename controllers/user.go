589b29ccdb4f0a79383508dd2d81cfb444cac624
AnjasFedo <fedoaf98@gmail.com>
2024-01-13 23:39:28 +0700

    Create README.md

--- controllers/user.go
+++ controllers/user.go
@@ -6,23 +6,27 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Anjasfedo/go-mongo/models"
-	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	"github.com/Anjasfedo/go-mongo/models" // Importing the models package which contains user model
+	"github.com/julienschmidt/httprouter"  // Importing the httprouter package for routing
+	"gopkg.in/mgo.v2"                      // Importing the mgo package for MongoDB interaction
+	"gopkg.in/mgo.v2/bson"                 // Importing bson package for working with BSON data
 )
 
+// UserController represents the controller for user-related operations
 type UserController struct {
-	session *mgo.Session
+	session *mgo.Session // MongoDB session
 }
 
+// NewUserController creates and returns a new UserController with the provided MongoDB session
 func NewUserController(s *mgo.Session) *UserController {
 	return &UserController{s}
 }
 
+// GetUserById handles GET requests to retrieve a user by ID
 func (uc UserController) GetUserById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	ID := p.ByName("id")
 
+	// Checking if the provided ID is a valid ObjectIdHex
 	if !bson.IsObjectIdHex(ID) {
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -32,43 +36,54 @@ func (uc UserController) GetUserById(w http.ResponseWriter, r *http.Request, p h
 
 	user := models.User{}
 
+	// Querying the database to find the user by ObjectId
 	if err := uc.session.DB("go-mongo").C("users").FindId(oid).One(&user); err != nil {
 		w.WriteHeader(404)
 		return
 	}
 
+	// Marshaling user object to JSON
 	userJson, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Setting response headers and writing the JSON response
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", userJson)
 }
 
+// CreateUser handles POST requests to create a new user
 func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	user := models.User{}
 
+	// Decoding the JSON request body into the user object
 	json.NewDecoder(r.Body).Decode(&user)
 
+	// Generating a new ObjectId for the user
 	user.ID = bson.NewObjectId()
 
+	// Inserting the user into the "users" collection in the database
 	uc.session.DB("go-mongo").C("users").Insert(user)
 
+	// Marshaling user object to JSON
 	userJson, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Setting response headers and writing the JSON response
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "%s\n", userJson)
 }
 
+// DeleteUserById handles DELETE requests to delete a user by ID
 func (uc UserController) DeleteUserById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	ID := p.ByName("id")
 
+	// Checking if the provided ID is a valid ObjectIdHex
 	if !bson.IsObjectIdHex(ID) {
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -76,26 +91,32 @@ func (uc UserController) DeleteUserById(w http.ResponseWriter, r *http.Request, 
 
 	oid := bson.ObjectIdHex(ID)
 
+	// Removing the user from the "users" collection in the database
 	if err := uc.session.DB("go-mongo").C("users").RemoveId(oid); err != nil {
 		w.WriteHeader(404)
 		return
 	}
-	
+
+	// Setting response status and writing a success message
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Deleted User with ID: %s\n", ID)
 }
 
+// GetUsers handles GET requests to retrieve all users
 func (uc UserController) GetUsers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var users []models.User
 
+	// Querying the database to retrieve all users from the "users" collection
 	if err := uc.session.DB("go-mongo").C("users").Find(bson.M{}).All(&users); err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
+	// Setting response headers and status
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(http.StatusOK)
 
+	// Iterating over users and writing JSON responses
 	for _, user := range users {
 		userJSON, err := json.Marshal(user)
 		if err != nil {
@@ -105,9 +126,11 @@ func (uc UserController) GetUsers(w http.ResponseWriter, r *http.Request, _ http
 	}
 }
 
+// UpdateUserById handles PUT requests to update a user by ID
 func (uc UserController) UpdateUserById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	ID := p.ByName("id")
 
+	// Checking if the provided ID is a valid ObjectIdHex
 	if !bson.IsObjectIdHex(ID) {
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -117,26 +140,31 @@ func (uc UserController) UpdateUserById(w http.ResponseWriter, r *http.Request, 
 
 	user := models.User{}
 
+	// Decoding the JSON request body into the user object
 	json.NewDecoder(r.Body).Decode(&user)
 
 	user.ID = oid
 
+	// Creating an update document for the user
 	updatedUser := bson.M{"$set": bson.M{
 		"name":   user.Name,
 		"gender": user.Gender,
 		"age":    user.Age,
 	}}
 
+	// Updating the user in the "users" collection in the database
 	if err := uc.session.DB("go-mongo").C("users").UpdateId(oid, updatedUser); err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
+	// Marshaling user object to JSON
 	userJson, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Setting response headers and writing the JSON response
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", userJson)
--- models/user.go
+++ models/user.go
@@ -1,12 +1,13 @@
 package models
 
 import (
-	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/bson" // Importing bson package for working with BSON data
 )
 
+// User represents the model for user data
 type User struct {
-	ID     bson.ObjectId `json:"_id" bson:"_id"`
-	Name   string        `json:"name" bson:"name"`
-	Gender string        `json:"gender" bson:"gender"`
-	Age    int           `json:"age" bson:"age"`
+	ID     bson.ObjectId `json:"_id" bson:"_id"`       // User's unique identifier
+	Name   string        `json:"name" bson:"name"`     // User's name
+	Gender string        `json:"gender" bson:"gender"` // User's gender
+	Age    int           `json:"age" bson:"age"`       // User's age
 }
