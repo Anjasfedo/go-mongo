c9065c391bb848da425840830810166d798ba368
AnjasFedo <fedoaf98@gmail.com>
2024-01-13 20:15:53 +0700

    CreateUser & DeleteUserById Method

--- controllers/user.go
+++ controllers/user.go
@@ -23,13 +23,14 @@ func (uc UserController) GetUserById(w http.ResponseWriter, r *http.Request, p h
 
 	if !bson.IsObjectIdHex(ID) {
 		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
-	oid := bson.ObjectIdHex(ID)
+	userID := bson.ObjectIdHex(ID)
 
 	user := models.User{}
 
-	if err := uc.session.DB("go-mongo").C("users").FindId(oid).One(&user); err != nil {
+	if err := uc.session.DB("go-mongo").C("users").FindId(userID).One(&user); err != nil {
 		w.WriteHeader(404)
 		return
 	}
@@ -44,10 +45,40 @@ func (uc UserController) GetUserById(w http.ResponseWriter, r *http.Request, p h
 	fmt.Fprintf(w, "%s\n", userJson)
 }
 
-func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request) {
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	user := models.User{}
 
+	json.NewDecoder(r.Body).Decode(&user)
+
+	user.ID = bson.NewObjectId()
+
+	uc.session.DB("go-mongo").C("users").Insert(user)
+
+	userJson, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application-json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", userJson)
 }
 
-func (uc UserController) DeleteUserById(w http.ResponseWriter, r *http.Request) {
+func (uc UserController) DeleteUserById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	ID := p.ByName("id")
 
+	if !bson.IsObjectIdHex(ID) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	userID := bson.ObjectIdHex(ID)
+
+	if err := uc.session.DB("go-mongo").C("users").RemoveId(userID); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Deleted user %s\n", userID)
 }
